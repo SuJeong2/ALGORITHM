58a9bd73dcfa004c846982e8017e43d675669d8f
sj971020 <sj971020@naver.com>
2020-10-07 17:36:27 +0900

    Algorithm - Bubble sort 2020/10/07

---
+++ Algorithm/BubbleSort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int max, min, temp;
+
+//Ascending
+void BubbleSort(int arr[], int n) {
+	for (int i = n - 1; i > 0; i--) {
+		
+		for (int j = 0; j < i; j++) {
+			if (arr[j] > arr[j + 1]){
+				temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+}
+
+void print_sort(int arr[], int n) {
+	for (int i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+}
+
+void main() {
+
+	int arr[10] = { 29,10,14,1,5,30,12,8,22,0 };
+	int arr2[10] = { 29,10,14,1,5,30,12,8,22,0 };
+
+	int n = sizeof(arr) / sizeof(int);
+
+	BubbleSort(arr, n);
+	printf("Bubble sort, Ascending, 오름차순: \n");
+	print_sort(arr, n);
+}
