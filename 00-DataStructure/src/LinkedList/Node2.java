9ceff3899e859e9e05d976016825d697caced1df
이수정 <lee.sujeong10111@gmail.com>
2021-02-13 15:00:55 +0900

    Linked List 구현

---
+++ 00-DataStructure/src/LinkedList/Node2.java
@@ -0,0 +1,29 @@
+package LinkedList;
+
+public class Node2 {
+	public String data;
+	public Node2 link;
+	
+	public Node2(String data, Node2 link) {
+		super();
+		this.data=data;
+		this.link=link;
+	}
+	
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public Node2 getLink() {
+		return link;
+	}
+	public void setLink(Node2 link) {
+		this.link = link;
+	}
+	
+	public String toString() {
+		return "Node [ data= "+this.data+", link= "+this.link+"]";
+	}
+}
