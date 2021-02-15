7ecb36009fdd68b582d8c3eca346729d81620fe1
이수정 <lee.sujeong10111@gmail.com>
2021-02-15 17:25:11 +0900

    Combination 조합 활용

---
+++ 01-백준/BJ_3020_백설공주와일곱난쟁이.java
@@ -0,0 +1,52 @@
+package com.ssafy.day0215;
+
+import java.util.Scanner;
+
+public class BJ_3020_백설공주와일곱난쟁이 {
+
+	static int[] nanjang, numbers;
+	static boolean flag;
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		nanjang = new int[9];
+		numbers = new int[9];
+		
+		for(int i=0; i<9; i++) {
+			nanjang[i] = sc.nextInt();
+		}
+		
+		flag = true;
+		comb(0,0);
+		
+		for(int i=0; i<7; i++)
+			System.out.println(nanjang[numbers[i]]);
+	}
+	
+	public static void comb(int cnt, int start) {
+		if(cnt==7) {
+			int sum=0;
+			for(int i=0; i<7; i++) {
+				sum+=nanjang[numbers[i]];
+			}
+			
+			if(sum==100) {
+				flag = false;
+				return;
+			}
+			
+			return;
+		}
+		
+		for(int i=start; i<9; i++) {
+			numbers[cnt]=i;
+			comb(cnt+1, i+1);
+			
+			if(flag==false)
+				break;
+		}
+	}
+
+}
